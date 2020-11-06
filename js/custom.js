3ba608be50a72947ba74576b3271deb6101af947
xapicella <46757976+xapicella@users.noreply.github.com>
2020-11-06 10:13:15 -0800

    Make nav collapse happen everywhere instantaneously

--- js/custom.js
+++ js/custom.js
@@ -7,13 +7,14 @@ $(window).load(function(){
 
 /* Mobile Navigation
     -----------------------------------------------*/
-$(window).scroll(function() {
+$(".navbar-fixed-top").addClass("top-nav-collapse");
+/*$(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
         $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-});
+});*/
 
 
 /* HTML document is loaded. DOM is ready. 
